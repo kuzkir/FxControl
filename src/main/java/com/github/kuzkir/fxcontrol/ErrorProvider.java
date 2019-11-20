ba9564119c67181df0aa7801a78ced118d12b51d
Kuznetsov Kirill <kuzkir89@ya.ru>
2019-11-20 11:47:37 +0300

    Initial commit

---
+++ src/main/java/com/github/kuzkir/fxcontrol/ErrorProvider.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.kuzkir.fxcontrol;
+
+import java.util.function.Supplier;
+import javafx.animation.FadeTransition;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author kuzkir
+ */
+public class ErrorProvider extends ImageView {
+    private Tooltip tooltip;
+    
+    public String getMessage() {
+        return tooltip.getText();
+    }
+    
+    public void setMessage(String message) {
+        Tooltip.uninstall(this, tooltip);
+        this.tooltip = new Tooltip(message);
+        Tooltip.install(this, tooltip);
+    }
+    
+    public ErrorProvider() {
+        super(new Image(ErrorProvider.class.getResourceAsStream("/High Importance-16.png"),16.0,16.0,true,true));
+        setVisible(false);
+    }
+    
+    public boolean verify(Supplier<Boolean> supplier, String message) {
+        setVisible(false);
+        if(supplier.get())
+            return true;
+        
+        setMessage(message);
+        slowShow();
+        return false;
+    }
+    
+    public boolean verify(Supplier<Boolean> supplier) {
+        setVisible(false);
+        if(supplier.get())
+            return true;
+        
+        slowShow();
+        return false;
+    }
+    
+    private void slowShow() {
+        setVisible(true);
+        FadeTransition ft = new FadeTransition(Duration.millis(500), this);
+        ft.setFromValue(1);
+        ft.setToValue(0);
+        ft.setCycleCount(2);
+        ft.setAutoReverse(true);
+        ft.play();
+        
+    }
+}
---
+++ src/main/java/com/github/kuzkir/fxcontrol/MessageBox.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.kuzkir.fxcontrol;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author kuzkir
+ */
+public class MessageBox {
+
+    public static boolean askQuestion(String operation, String question) {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+        try {
+            InputStream in = MessageBox.class.getResourceAsStream("/Help-30.png");
+            ImageView iv = new ImageView(new Image(in));
+            a.setGraphic(iv);
+            a.initStyle(StageStyle.UTILITY);
+        } catch (Exception ex) {
+        }
+        a.setTitle(operation);
+        a.setHeaderText(question);
+
+        ButtonType btnYes = new ButtonType("Да", ButtonData.YES);
+        ButtonType btnNo = new ButtonType("Нет", ButtonData.NO);
+
+        a.getButtonTypes().setAll(btnYes,btnNo);
+
+        Optional<ButtonType> result = a.showAndWait();
+        if (result.get().equals(btnYes)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void showException(String operation, Exception e) {
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        try {
+            InputStream in = MessageBox.class.getResourceAsStream("/High Importance-30.png");
+            ImageView iv = new ImageView(new Image(in));
+            a.setGraphic(iv);
+            a.initStyle(StageStyle.UTILITY);
+        } catch (Exception ex) {
+        }
+
+        a.setHeaderText(e.getClass().getSimpleName());
+        a.setContentText(e.getMessage());
+        a.setTitle(operation);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String exceptionText = sw.toString();
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(textArea, 0, 0);
+
+        a.getDialogPane().setExpandableContent(expContent);
+
+        a.showAndWait();
+    }
+    
+    public static void showWarning(String operation, String text) {
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        try {
+            InputStream in = MessageBox.class.getResourceAsStream("/Error-30.png");
+            ImageView iv = new ImageView(new Image(in));
+            a.setGraphic(iv);
+            a.initStyle(StageStyle.UTILITY);
+        } catch (Exception ex) {
+        }
+
+        a.setHeaderText(text);
+        a.setTitle(operation);
+        
+        a.showAndWait();
+    }
+    
+    public static void showInfo(String operation, String text) {
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        try {
+            InputStream in = MessageBox.class.getResourceAsStream("/Info-30.png");
+            ImageView iv = new ImageView(new Image(in));
+            a.setGraphic(iv);
+            a.initStyle(StageStyle.UTILITY);
+        } catch (Exception ex) {
+        }
+
+        a.setHeaderText(text);
+        a.setTitle(operation);
+        
+        a.showAndWait();
+    }
+}
